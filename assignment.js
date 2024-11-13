d00c69db5e60d945b051d146f1ed608b5ab99417
ALEXANDER <msumba83@gmail.com>
2024-11-13 22:17:16 +0200

    Done values and variables

---
+++ assignment.js
@@ -0,0 +1,6 @@
+let country = "Malawi";
+let continent = "Africa";
+let population = 18000000
+
+
+console.log(country, continent, population);
---
+++ script.js
@@ -0,0 +1,4 @@
+// VALUES AND VARIABLES 
+// https://jonas.io/assignments/javascript-fundamentals-part-1.html
+let firstName = "Alexander";
+console.log(firstName);
