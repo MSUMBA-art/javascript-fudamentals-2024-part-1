52e5a40e05b3d8a6ef3e40ae8edd5105da72c279
ALEXANDER <msumba83@gmail.com>
2024-11-15 06:12:16 +0200

    done data types

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// VALUES AND VARIABLES 
-// https://jonas.io/assignments/javascript-fundamentals-part-1.html
-let firstName = "Alexander";
-console.log(firstName);
+// // VALUES AND VARIABLES 
+// // https://jonas.io/assignments/javascript-fundamentals-part-1.html
+// let firstName = "Alexander";
+// console.log(firstName);
+
+// // BOOLEANS 
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 123);
+// console.log(typeof "Alex");
+
+// javascriptIsFun = "Manchester united"
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2024;
+// console.log(typeof year);
+
+
+
+
+
+
+
+
+
