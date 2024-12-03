8ee52ab8805ce76665a809be4eb2efd3c9b29c77
ALEXANDER <msumba83@gmail.com>
2024-12-03 18:20:24 +0200

    done basic operators and assignments

--- assignment.js
+++ assignment.js
@@ -11,12 +11,12 @@
 
  2. Log the types of isIsland, population, country and language to the console.*/
 
-let isIsland = false;
-let language;
+// let isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
 
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// // VALUES AND VARIABLES 
+// // VALUES AND VARIABLES
 // // https://jonas.io/assignments/javascript-fundamentals-part-1.html
 // let firstName = "Alexander";
 // console.log(firstName);
 
-// // BOOLEANS 
+// // BOOLEANS
 // let javascriptIsFun = true;
 // console.log(javascriptIsFun);
 // console.log(typeof javascriptIsFun);
@@ -22,6 +22,43 @@
 // year = 2024;
 // console.log(typeof year);
 
+// LET, CONST & VAR
+//    1. let
+/*let age = 30;
+age = 31;
+console.log(age);
+ 
+//   2. const 
+const birthYear = 1983;
+birthYear = 1988*/
+
+//   3. var
+// avoid using var
+
+// BASIC OPERATORS
+const now = 2024
+const ageAlex = now - 1983;
+const ageBertha = now - 1981
+console.log(ageAlex, ageBertha);
+
+console.log(ageAlex / 2, ageAlex * 2, ageBertha * + 2, ageBertha - ageAlex);
+
+const firstName = 'Alexander';
+const lastName = 'Msumba';
+console.log(firstName + " " + lastName);
+
+// ASSIGNMENT OPERATORS
+let sum = 9 + 12; //21
+sum += 10 // sum = sum + 10 = 31
+sum *= 4 // sum = sum * 4 = 124
+sum++;
+sum--;
+console.log(sum);
+
+console.log(ageAlex >= ageBertha);
+
+const isFullAge = ageAlex >= 21;
+console.log(isFullAge);
 
 
 
